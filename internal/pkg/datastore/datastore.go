77c9857908a1a32d7cb04155b430b873474d32a2
Rui Constancio <r.miguel.a.c@gmail.com>
2023-03-08 07:13:04 -0300

    Add database singleton and config changes

--- internal/pkg/datastore/datastore.go
+++ internal/pkg/datastore/datastore.go
@@ -1,6 +1,33 @@
 package datastore
 
-type Config struct {
-	Host string `json:"host"`
-	Port string `json:"port"`
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/rmiguelac/tasker/internal/configs"
+)
+
+type dbConn struct {
+	Conn *sql.DB
+}
+
+var db *dbConn
+
+func New() *dbConn {
+	if db == nil {
+		connString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+			configs.DBConfig.Username,
+			configs.DBConfig.Password,
+			configs.DBConfig.Hostname,
+			configs.DBConfig.Port,
+			configs.DBConfig.Database,
+		)
+		conn, err := sql.Open("postgres", connString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return &dbConn{Conn: conn}
+	}
+	return db
 }
