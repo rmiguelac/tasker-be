19d3072d7d50bacedbef75ea73eb6177fdbe1bf5
Rui Constancio <r.miguel.a.c@gmail.com>
2023-03-11 11:00:04 -0300

    Change routing to gorilla

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+
 	"github.com/rmiguelac/tasker/internal/pkg/datastore"
 )
 
@@ -15,9 +17,27 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/tasks", createTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id}", getTaskHandler).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", r))
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/tasks", createTaskHandler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func getTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	db := datastore.New()
+	row := db.Conn.QueryRow("SELECT * FROM tasks WHERE id=?", vars["id"])
+
+	err := row.Scan()
+	if err != nil {
+		fmt.Printf("Unable to scan query results: %s", err)
+	}
+
+	fmt.Fprintf(w, "Values are: %s", row)
 }
 
 func createTaskHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,20 +47,18 @@ func createTaskHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Parsing form failed: %v", err)
 	}
 
-	i := `insert into "tasks" ("title") values (r.FormValue("title"))`
+	i := `INSERT INTO tasks (title) VALUES ($1) RETURNING id`
 	db := datastore.New()
-	rs, err := db.Conn.Exec(i)
+
+	var id int
+
+	db.Conn.QueryRow(i, r.FormValue("title")).Scan(&id)
 	if err != nil {
 		log.Println("Unable to insert into the database")
 		log.Println(err)
 	}
 
-	lid, err := rs.LastInsertId()
-	if err != nil {
-		log.Fatal("Unable to get last inserted ID from created task")
-	}
-
-	fmt.Fprintf(w, "Task with id %d created.", lid)
+	fmt.Fprintf(w, "Task with id %d created.", id)
 }
 
 func main() {
