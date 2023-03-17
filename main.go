14ded74c527bb3020bcd5d25ad0b0e8b669a7ca4
Rui Constancio <r.miguel.a.c@gmail.com>
2023-03-17 09:39:52 -0300

    Move handlers to http package

--- main.go
+++ main.go
@@ -1,71 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strings"
-
-	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 
-	"github.com/rmiguelac/tasker/internal/pkg/datastore"
-	"github.com/rmiguelac/tasker/internal/tasks"
+	tserver "github.com/rmiguelac/tasker/internal/server/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome!")
-	fmt.Println("Endpoint hit: homePage")
-}
-
-func handleRequests() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/tasks", createTaskHandler).Methods("POST")
-	r.HandleFunc("/tasks/{id}", getTaskHandler).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", r))
-	http.HandleFunc("/", homePage)
-}
-
-func getTaskHandler(w http.ResponseWriter, r *http.Request) {
-
-	vars := mux.Vars(r)
-	task := tasks.Task{}
-
-	db := datastore.New()
-	row := db.Conn.QueryRow("SELECT id, title FROM tasks WHERE id=$1", vars["id"])
-
-	err := row.Scan(&task.Id, &task.Title)
-	if err != nil {
-		fmt.Printf("Unable to scan query results: %s", err)
-		return
-	}
-
-	fmt.Fprintf(w, "Task %d: '%s' created", task.Id, strings.Trim(task.Title, " "))
-}
-
-func createTaskHandler(w http.ResponseWriter, r *http.Request) {
-
-	err := r.ParseForm()
-	if err != nil {
-		fmt.Fprintf(w, "Parsing form failed: %v", err)
-	}
-
-	i := `INSERT INTO tasks (title) VALUES ($1) RETURNING id`
-	db := datastore.New()
-
-	var id int
-
-	db.Conn.QueryRow(i, r.FormValue("title")).Scan(&id)
-	if err != nil {
-		log.Println("Unable to insert into the database")
-		log.Println(err)
-	}
-
-	fmt.Fprintf(w, "Task with id %d created.", id)
-}
-
 func main() {
-	handleRequests()
+	tserver.HandleRequests()
 
 }
