bf11fcc760d5da0e9956a59df3208ea84eac5e26
Rui Constancio <r.miguel.a.c@gmail.com>
2023-04-01 11:25:29 -0300

    CORS

--- internal/api/tasks.go
+++ internal/api/tasks.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -32,17 +33,43 @@ func New(listenAddr string) *APIServer {
 	}
 }
 
+func CORSHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		// Handle OPTIONS requests
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte{})
+			return
+		}
+
+		// Call the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
+}
+
 // Run starts the http server with a mux router
 // as well as task handlers
 func (s *APIServer) Run() {
 	r := mux.NewRouter()
+	r.Use(CORSHandler)
 
 	r.HandleFunc("/tasks", s.HandleCreateTask).Methods("POST")
 	r.HandleFunc("/tasks/{id}", s.HandleGetTask).Methods("GET")
 	r.HandleFunc("/tasks/{id}", s.HandleUpdateTask).Methods("PUT")
 	r.HandleFunc("/tasks/{id}", s.HandleDeleteTask).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(s.listenAddr, r))
 
+	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte{})
+	}).Methods("OPTIONS")
+
+	log.Fatal(http.ListenAndServe(s.listenAddr, r))
 }
 
 // HandleGetTask takes the id variable from the request
@@ -114,17 +141,16 @@ func (s *APIServer) HandleUpdateTask(w http.ResponseWriter, r *http.Request) {
 
 	t, err := s.datastore.UpdateTask(id, &task)
 	if err != nil {
-		log.Printf("Unable to update task: %s\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-
-	if t == nil {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "Task with id %d not found.", id)
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "Task with id %d not found.", id)
+		} else {
+			log.Printf("Unable to update task: %s\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	}
 
 	w.Header().Set("Content-Type", "applicaton/json")
-	w.WriteHeader(http.StatusOK)
 	err = json.NewEncoder(w).Encode(t)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -134,17 +160,25 @@ func (s *APIServer) HandleUpdateTask(w http.ResponseWriter, r *http.Request) {
 
 func (s *APIServer) HandleDeleteTask(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("Content-Type", "applicaton/json")
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
 		log.Printf("Unable to parse task id: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
-	if err := s.datastore.DeleteTask(id); err != nil {
-		// TODO: If id does not exist, what happens?
-		log.Printf("Unable to delete task: %s\n", err)
+	err = s.datastore.DeleteTask(id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Fprintf(w, "Unable to delete task %d. No such task", id)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		} else {
+			fmt.Printf("Unable to delete task: %s\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
-
-	w.Header().Set("Content-Type", "applicaton/json")
-	w.WriteHeader(http.StatusNoContent)
 }
